2562fda6dd9f796f5fcdf6cb585a761c057f472d
CarlosMoralesR <carlangetas007@hotmail.com>
2022-04-15 23:38:13 -0500

    Funcionalidad principal del juego

--- app.js
+++ app.js
@@ -22,6 +22,39 @@ const gameDiv = document.querySelector ('.game');
 const resultsDiv = document.querySelector ('.results');
 const resultDivs = document.querySelectorAll ('.results_result');
 
+choiceButtons.forEach( button => {
+    button.addEventListener("click", () => {
+        const choiceName = button.dataset.choice;
+        const choice = CHOICES.find(choice => choice.name === choiceName);
+        choose(choice);
+    })
+})
+
+function choose(choice) {
+    const aichoice = aiChoose();
+    displayResults ([choice, aichoice]);
+}
+
+function aiChoose() {
+    const rand = Math.floor(Math.random() * CHOICES.length);
+    return CHOICES[rand];
+}
+
+function displayResults(results) {
+    resultDivs.forEach((resultDiv, idx) => {
+        setTimeout(() => {
+            resultDiv.innerHTML = `
+                <div class="choice ${results[idx].name}">
+                    <img src="images/icon-${results[idx].name}.svg" alt="${results[idx].name}" />
+                </div>
+            `
+        }, idx * 1000);
+    });
+
+    gameDiv.classList.toggle("hidden");
+    resultsDiv.classList.toggle("hidden");
+}
+
 btnRules.addEventListener("click", function(){
     modalRules.classList.toggle("show-modal");
 });
