ec7df729b2f9417f3cb5331f8d5eed01e6b8691e
CarlosMoralesR <carlangetas007@hotmail.com>
2022-04-15 23:23:32 -0500

    Agregando estilos y js

--- app.js
+++ app.js
@@ -2,6 +2,26 @@ const btnRules = document.querySelector('.rules-btn');
 const btnClose = document.querySelector('.close-btn');
 const modalRules = document.querySelector ('.modal');
 
+const CHOICES = [
+{
+    name: "paper",
+    beats: "rock"
+},
+{
+    name: "scissors",
+    beats: "paper"
+},
+{
+    name: "rock",
+    beats: "scissors"
+}
+]
+
+const choiceButtons = document.querySelectorAll ('.choice-btn');
+const gameDiv = document.querySelector ('.game');
+const resultsDiv = document.querySelector ('.results');
+const resultDivs = document.querySelectorAll ('.results_result');
+
 btnRules.addEventListener("click", function(){
     modalRules.classList.toggle("show-modal");
 });
